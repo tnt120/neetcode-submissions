afc77cc9bc2bf44f8ba86199a999a5a8a0db9683
tnt120 <48412587+tnt120@users.noreply.github.com>
2026-05-21 00:16:56 +0200

    Add: merge-intervals - submission-1

---
+++ Data Structures & Algorithms/merge-intervals/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals) {
+        intervals.sort((a, b) => a[0] - b[0]);
+
+        for (let i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i + 1][0] <= intervals[i][1]) {
+                intervals[i][1] = Math.max(intervals[i][1], intervals[i + 1][1]);
+                intervals.splice(i + 1, 1);
+                i--
+            }
+        }
+
+        return intervals;
+    }
+}
