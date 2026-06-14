60ef035e3e9d2ce088bc4411a96588806668c149
tnt120 <48412587+tnt120@users.noreply.github.com>
2026-06-15 01:38:25 +0200

    Add: three-integer-sum - submission-15

---
+++ Data Structures & Algorithms/three-integer-sum/submission-15.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        const res = [];
+        let left;
+        let right;
+
+        nums.sort((a, b) => a - b);
+
+        for (let i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+            left = i + 1;
+            right = nums.length - 1;
+
+            while (left < right)  {
+                const sum = nums[i] + nums[left] + nums[right];
+
+                if (sum === 0) {
+                    res.push([nums[i], nums[left], nums[right]]);
+
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] === nums[left - 1]) left++;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+
+        return res;
+    }
+}
